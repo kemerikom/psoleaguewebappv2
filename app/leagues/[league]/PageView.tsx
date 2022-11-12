2c33ece4b6d83d1b08aa23e00795e6bad31477c1
FlyingTurkman <dytdenizarrli@hotmail.com>
2022-11-12 09:36:13 +0300

    master

--- app/leagues/[league]/PageView.tsx
+++ app/leagues/[league]/PageView.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { leagueName } from "../../../typings"
 import { Tab } from '@headlessui/react'
+import { Disclosure } from '@headlessui/react'
+import {IoChevronDownOutline} from 'react-icons/io5'
 
 
 export default function PageView({data}:{data:leagueName}){
     return(
         <div className="flex w-full p-2 rounded">
-            <Tab.Group as={'div'}>
+            <Tab.Group as={'div'} className='flex flex-col space-y-2'>
                 <Tab.List as={'div'} className='flex flex-row bg-white p-1 rounded space-x-2'>
                     <Tab as='div' className={'outline-none'}>
                         {({selected})=>(
@@ -25,8 +27,20 @@ export default function PageView({data}:{data:leagueName}){
                     </Tab>
                 </Tab.List>
                 <Tab.Panels>
-                    <Tab.Panel>
-                        Önceki sezonlar
+                    <Tab.Panel as='div' className={'flex flex-col bg-white rounded p-2'}>
+                        <Disclosure as='div' className={'w-full'}>
+                            {({open})=>(
+                                <div className="flex flex-col w-full space-y-2">
+                                    <Disclosure.Button as='button' className={`${open?'bg-blue-600 text-white':'bg-blue-100 text-black'} flex flex-row items-center justify-between w-full p-1 rounded transition-all outline-none`}>
+                                        2020-2021 Sezonu
+                                        <IoChevronDownOutline className={`text-2xl ${open?'rotate-180':'rotate-0'} transition-all`}></IoChevronDownOutline>
+                                    </Disclosure.Button>
+                                    <Disclosure.Panel as='div' className={`flex bg-blue-100 p-1 rounded transition-all`}>
+                                        Sezon bilgisi
+                                    </Disclosure.Panel>
+                                </div>
+                            )}
+                        </Disclosure>
                     </Tab.Panel>
                     <Tab.Panel>
                         Şimdiki sezon
