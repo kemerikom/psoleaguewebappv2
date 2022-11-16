02abb503507bf8b7547f4c2598a950ae334b8d79
FlyingTurkman <dytdenizarrli@hotmail.com>
2022-11-16 06:27:04 +0300

    master

--- app/MainMenu.tsx
+++ app/MainMenu.tsx
@@ -4,22 +4,30 @@ import { Menu } from '@headlessui/react'
 
 export default function MainMenu() {
     return(
-        <div className="flex flex-row h-10 w-full items-center bg-black bg-opacity-5 backdrop-blur-sm justify-start hover:bg-blue-800 text-white px-2 transition-all">
-            <div className="flex flex-row">
-                <Link className='flex mx-1' href={'/'}>PSO League</Link>
-                <Link className='flex mx-1' href={'/leagues'}>Leagues</Link>
-                <Link className='flex mx-1' href={'/'}>Teams</Link>
-                <Link className='flex mx-1' href={'/'}>Players</Link>
+        <div className="flex flex-row group h-10 sticky top-0 left-0 z-50 w-full items-center bg-black bg-opacity-10 backdrop-blur-sm justify-start hover:bg-blue-800 text-white px-2 transition-all">
+            <div className="flex flex-row h-full">
+                <Link className='flex mx-1 h-full p-2 hover:bg-blue-900 transition-all' href={'/'}>PSO League</Link>
+                <Link className='flex mx-1 h-full p-2 hover:bg-blue-900 transition-all' href={'/leagues'}>Leagues</Link>
+                <Link className='flex mx-1 h-full p-2 hover:bg-blue-900 transition-all' href={'/'}>Teams</Link>
+                <Link className='flex mx-1 h-full p-2 hover:bg-blue-900 transition-all' href={'/'}>Players</Link>
             </div>
             <div className='flex flex-row ml-auto h-full items-center justify-start'>
-                <div className='flex flex-col'>
+                <div className='flex flex-col h-full'>
                     <Menu>
-                        <Menu.Button>Profile</Menu.Button>
-                        <Menu.Items className={'absolute bg-slate-800 top-10 right-2 p-3 flex flex-col items-end gap-y-2 rounded-b'}>
-                            <Menu.Item><Link href={'/'}>My Profile</Link></Menu.Item>
-                            <Menu.Item><Link href={'/'}>Settings</Link></Menu.Item>
-                            <Menu.Item><Link href={'/'}>My Profile</Link></Menu.Item>
-                            <Menu.Item><Link href={'/'}>My Profile</Link></Menu.Item>
+                        <Menu.Button className={'hover:bg-blue-900 transition-all h-full mx-2 p-2'}>Profile</Menu.Button>
+                        <Menu.Items as='div' className={'absolute bg-black bg-opacity-10 backdrop-blur-sm group-hover:bg-blue-800 hover:bg-blue-800 transition-all top-10 right-2 flex flex-col items-end gap-y-2 rounded-b'}>
+                            <Menu.Item>
+                                <Link className='hover:bg-blue-900 transition-all py-2 px-5 w-full' href={'/'}>My Profile</Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link className='hover:bg-blue-900 transition-all py-2 px-5 w-full' href={'/'}>Settings</Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link className='hover:bg-blue-900 transition-all py-2 px-5 w-full' href={'/'}>My Profile</Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link className='hover:bg-blue-900 transition-all py-2 px-5 w-full' href={'/'}>My Profile</Link>
+                            </Menu.Item>
                         </Menu.Items>
                     </Menu>
                 </div>
--- app/leagues/[league]/TeamsPage.tsx
+++ app/leagues/[league]/TeamsPage.tsx
@@ -2,12 +2,51 @@ import { teamsType } from "../../../typings"
 
 export default function TeamsPage({teams}:{teams:teamsType[]}){
     return(
-        <div className="flex flex-col">
-            <div className="flex flex-row">
-                <div className="flex items-center w-16 aspect-square rounded-full">
-                    <img className="rounded-full" src='/teamlogo.png'></img>
+        <div className="flex flex-row flex-wrap justify-center">
+            {teams?.map((team)=>{
+                let bs= Math.floor(Math.random()*(350))+150
+                return(
+                <div key={team.name} id='teamBack' className="flex flex-1 flex-row flex-shrink-0 m-1 p-2 rounded cursor-pointer transition-all">
+                <style jsx>
+                    {`
+                        #teamBack{
+                            background: linear-gradient(-45deg, ${team.color1}, ${team.color2},${team.color1}, ${team.color2});
+                            background-size: ${bs}% ${bs}%;
+                            animation: gradient 5s ease infinite;
+                        }
+                        @keyframes gradient{
+                            0% {
+                                background-position: 0% 50%;
+                            }
+                            50% {
+                                background-position: 100% 50%;
+                            }
+                            100% {
+                                background-position: 0% 50%;
+                            }
+                        }
+                    `}
+                </style>
+                    <div className="flex flex-row space-x-2 items-center">
+                        <div className="flex items-center w-16 aspect-square rounded-full">
+                            <img className="rounded-full" src='/teamlogo.png'></img>
+                        </div>
+                        <div className="flex flex-col cursor-pointer">
+                            <h3
+                            style={{color:team.fontcolor}}
+                            >
+                                <b>Team Name:</b> {team.name}
+                            </h3>
+                            <h3
+                            style={{color:team.fontcolor}}
+                            >
+                                <b>Team Tag:</b> {team.shortname}
+                            </h3>
+                        </div>
+                    </div>
                 </div>
-            </div>
+                )
+            })}
         </div>
     )
 }
