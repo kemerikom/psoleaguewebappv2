779cc1acd4271be46f6b722a718b59f39d320669
FlyingTurkman <dytdenizarrli@hotmail.com>
2022-11-21 17:10:04 +0300

    master

--- app/teams/[team]/TrophyRoom.tsx
+++ app/teams/[team]/TrophyRoom.tsx
@@ -1,70 +1,10 @@
-import {AiTwotoneTrophy} from 'react-icons/ai'
-import {BsFillTrophyFill} from 'react-icons/bs'
+import Trophy from "./Trophy"
+
 
 export default function TrophyRoom({color1,color2}:{color1:string,color2:string}){
     return (
         <div className="flex flex-row items-start justify-start">
-            
-            <div id='box' className="flex flex-col p-2 rounded items-center m-2 transition-all">
-                <style jsx>
-                    {
-                        `
-                        #box{
-                            box-shadow:
-                            inset 0 0 60px whitesmoke,
-                            inset 20px 0 80px ${color1},
-                            inset -20px 0 80px ${color2},
-                            inset 20px 0 300px ${color1},
-                            inset -20px 0 300px ${color2},
-                            0 0 50px #fff,
-                            -10px 0 80px ${color1},
-                            10px 0 80px ${color2};
-                            animation: animateGlow 5s ease infinite;
-                        }
-
-                        @keyframes animateGlow {
-                            0% {
-                                box-shadow:
-                                inset 0 0 60px whitesmoke,
-                                inset 20px 0 60px ${color1},
-                                inset -20px 0 60px ${color2},
-                                inset 20px 0 200px ${color1},
-                                inset -20px 0 200px ${color2},
-                                0 0 50px #fff,
-                                -10px 0 80px ${color1},
-                                10px 0 80px ${color2};
-                            }
-                            50% {
-                                box-shadow:
-                                inset 0 0 60px whitesmoke,
-                                inset 20px 0 60px ${color2},
-                                inset -20px 0 60px ${color1},
-                                inset 20px 0 200px ${color2},
-                                inset -20px 0 200px ${color1},
-                                0 0 50px #fff,
-                                -10px 0 80px ${color2},
-                                10px 0 80px ${color1};
-                            }
-                            100% {
-                                box-shadow:
-                                inset 0 0 60px whitesmoke,
-                                inset 20px 0 60px ${color1},
-                                inset -20px 0 60px ${color2},
-                                inset 20px 0 200px ${color1},
-                                inset -20px 0 200px ${color2},
-                                0 0 50px #fff,
-                                -10px 0 80px ${color1},
-                                10px 0 80px ${color2};
-                            }
-                          }
-                        `
-                    }
-                </style>
-                <BsFillTrophyFill  className='text-9xl text-gold'/>
-                <div className='flex m-2 p-2 rounded bg-white backdrop-blur-sm bg-opacity-40'>
-                    <h3 className='font-bold'>Türkiye 1. Lig Şampiyonu</h3>
-                </div>
-            </div>
+            <Trophy color1={color1} color2={color2}/>
         </div>
     )
 }
