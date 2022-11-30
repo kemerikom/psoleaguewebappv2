44e5fb2c5651878bdab2322b5f4c99db90ce3e37
FlyingTurkman <99084104+FlyingTurkman@users.noreply.github.com>
2022-11-30 19:07:15 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,15 +5,14 @@ module.exports = {
     appDir:true
   },
   env:{
-    apiKey: 'AIzaSyAplg7vVFBiwId0TjKXUgeinCyLQczchug',
-    authDomain: 'psoleaguev2.firebaseapp.com',
-    projectId: 'psoleaguev2',
-    storageBucket: 'psoleaguev2.appspot.com',
-    messagingSenderId: '620696940492',
-    appId: '1:620696940492:web:b3b404af9e282a72fcf769',
-    measurementId: 'G-MPEQ62XY32',
-    mongoUri:"mongodb+srv://PSOLeagueMod:005LorD005@psoleague.wuceh5w.mongodb.net/?retryWrites=true&w=majority",
+    apiKey: 'apiKey',
+    authDomain: 'authDomain',
+    projectId: 'projectId',
+    storageBucket: 'storageBucket',
+    messagingSenderId: 'messagingSenderId',
+    appId: 'appId',
+    measurementId: 'measurementId',
+    mongoUri:" mongoUri",
     appPath:'http://localhost:3000',
-    psoApiKey:""
   }
 }
