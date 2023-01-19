54a613bb8497fc1629de2ee5473668358dab6520
FlyingTurkman <dytdenizarrli@hotmail.com>
2023-01-19 20:40:41 +0300

    master

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,7 +4,7 @@ import {IoSearchOutline} from 'react-icons/io5'
 export default function SearchBar({value,setValue,goSearch}:{value:string,setValue:Dispatch<SetStateAction<string>>,goSearch:Dispatch<SetStateAction<boolean>>}){
     return(
         <div className="flex flex-row items-center justify-center bg-white py-1 px-3 rounded-full mx-auto space-x-1">
-            <input className='rounded-full bg-transparent' maxLength={20} value={value} onChange={(e)=>setValue(e.currentTarget.value)} onKeyDown={(e)=>keyDown(e)}  type={'search'} pattern="[A-Za-z]" placeholder="Search a team..."/>
+            <input className='rounded-full bg-transparent' maxLength={20} value={value} onChange={(e)=>setValue(e.currentTarget.value)} onKeyDown={(e)=>keyDown(e)}  type={'search'} pattern="[A-Za-z]" placeholder="Search..."/>
             <div onClick={search}>
                 <IoSearchOutline className='text-xl cursor-pointer'/>
             </div>
