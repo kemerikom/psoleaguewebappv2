2fbd91b283342948a1a8e179a736940b744fb0da
FlyingTurkman <dytdenizarrli@hotmail.com>
2023-01-20 00:56:38 +0300

    master

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,17 +6,20 @@ import { useEffect, useState } from 'react'
 import { onAuthStateChanged} from 'firebase/auth'
 import { logoutUser } from '../utils/firebase/logoutUser'
 import {auth} from '../utils/firebase/config'
+import { notificationType } from '../typings'
 
 
 export default function RootLayout({children}:{children: React.ReactNode}) {
   const [user,setUser]=useState<boolean>(false)
   const [uid,setUid]=useState<string|null>(null)
   const [login,setLogin]=useState<boolean>(false)
-
+  const [notifications,setNotifications]=useState<notificationType[]>([])
   const siteData ={
     user,
     uid,
-    login
+    login,
+    notifications,
+    setNotifications
   }
 /*   try {
     onAuthStateChanged(auth,(user)=>{
